c27874b445ee4f12ca844a1010469ed378e37c4f
shaandesaiqb <shaandesai@quantumblack.com>
2020-08-03 19:02:42 +0200

    code to run hamiltonian double pend learning

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ import torch.nn as nn
 import torch
 import numpy as np
 from typing import Union, Iterable, Tuple, Dict
+
 torch.pi = torch.tensor(np.pi)
+
 
 def attach_dim(v, n_dim_to_prepend=0, n_dim_to_append=0):
     return v.reshape(
         torch.Size([1] * n_dim_to_prepend)
         + v.shape
         + torch.Size([1] * n_dim_to_append))
+
 
 def tensor(v: Union[float, np.ndarray, torch.Tensor],
            min_ndim=1,
@@ -37,6 +40,7 @@ def tensor(v: Union[float, np.ndarray, torch.Tensor],
                          + torch.Size([1] * (min_ndim - v.ndimension())))
     return v
 
+
 class MODEL(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_size, assume_canonical_coords=True,
                  field_type='solenoidal'):
@@ -47,24 +51,24 @@ class MODEL(nn.Module):
         self.mlp1 = nn.Linear(input_dim, hidden_dim)
         self.mlp2 = nn.Linear(hidden_dim, hidden_dim)
         self.mlp3 = nn.Linear(hidden_dim, output_size, bias=None)
-        self.nonlin = torch.tanh
+        self.nonlin = torch.nn.ReLU()
 
         for l in [self.mlp1, self.mlp2, self.mlp3]:
             torch.nn.init.orthogonal_(l.weight)
+        #
+        # self.mat1 = nn.Linear(2, 10)
+        # self.mat2 = nn.Linear(10, 10)
+        # self.mat3 = nn.Linear(10, 4)
+        #
+        # self.M = None
+        # self.Minv = None
 
-        self.mat1 = nn.Linear(2, 10)
-        self.mat2 = nn.Linear(10, 10)
-        self.mat3 = nn.Linear(10, 4)
-
-        self.M = None
-        self.Minv = None
-
-    def rk4(self, dx_dt_fn, x_t, dt):
-        k1 = dt * dx_dt_fn(x_t)
-        k2 = dt * dx_dt_fn(x_t + (1 / 2) * k1)
-        k3 = dt * dx_dt_fn(x_t + (1 / 2) * k2)
-        k4 = dt * dx_dt_fn(x_t + k3)
-        x_tp1 = x_t + (1 / 6) * (k1 + k2 * 2 + k3 * 2 + k4)
+    def rk4(self, dx_dt_fn, x_t):
+        k1 = self.dt * dx_dt_fn(x_t)
+        k2 = self.dt * dx_dt_fn(x_t + (1. / 2) * k1)
+        k3 = self.dt * dx_dt_fn(x_t + (1. / 2) * k2)
+        k4 = self.dt * dx_dt_fn(x_t + k3)
+        x_tp1 = x_t + (1. / 6) * (k1 + k2 * 2. + k3 * 2. + k4)
         return x_tp1
 
     def M_matrix(self, q):
@@ -74,6 +78,7 @@ class MODEL(nn.Module):
         self.M = self.block_diag(out.reshape(-1, 2, 2))
         self.Minv = torch.inverse(self.M)
         return self.M
+
     def block_diag(self, m):
         """
         Make a block diagonal matrix along dim=-3
@@ -106,42 +111,40 @@ class MODEL(nn.Module):
 
         return fin
 
-    def rebase(self,q):
-
+    def rebase(self, q):
         return (q + torch.pi) % (2 * torch.pi) - torch.pi
-    def next_step(self, x):
 
+    def next_step(self, x):
         # step 1, get p
-        _ = self.M_matrix(self.rebase(x[:,:2]))
-        p_init = (self.M@(x[:,2:].reshape(-1,1))).reshape(-1,2)
+        # _ = self.M_matrix(self.rebase(x[:, :2]))
+        # p_init = (self.M @ (x[:, 2:].reshape(-1, 1))).reshape(-1, 2)
         # L = self.forward(x)  # traditional forward pass
         # dlds = torch.autograd.grad(L.sum(), x, create_graph=True)[0]
         # p_init = dlds[:, 2:]
-        q_init = self.rebase(x[:, :2])
-        init_param = torch.cat([q_init, p_init], 1)
-        next_set = self.rk4(self.time_deriv, init_param,self.dt)
-        qdot = (self.Minv @ (next_set[:, 2:].reshape(-1, 1))).reshape(-1, 2)
-        return torch.cat([self.rebase(next_set[:,:2]),qdot],1)
+        # q_init = self.rebase(x[:, :2])
+        # init_param = torch.cat([q_init, p_init], 1)
+        init_param = torch.cat([self.rebase(x[:,:2]),x[:,2:]],1)
+        next_set = self.rk4(self.time_deriv, init_param)
+        # qdot = (self.Minv @ (next_set[:, 2:].reshape(-1, 1))).reshape(-1, 2)
+        return next_set #torch.cat([self.rebase(next_set[:, :2]), qdot], 1)
 
-    def Lpartials(self,x):
+    def Lpartials(self, x):
         F2 = self.forward(x)  # traditional forward pass
         dF2 = torch.autograd.grad(F2.sum(), x, create_graph=True)[0]  # gradients for solenoidal field
         return dF2
-    def Hpartials(self,x):
+
+    def Hpartials(self, x):
         F2 = self.forward(x)  # traditional forward pass
         dF2 = torch.autograd.grad(F2.sum(), x, create_graph=True)[0]  # gradients for solenoidal field
-        return torch.cat([dF2[:,2:],-dF2[:,:2]],1)
+        return torch.cat([dF2[:, 2:], -dF2[:, :2]], 1)
+
     def time_deriv(self, x, p=False, separate_fields=False):
-        '''NEURAL ODE-STLE VECTOR FIELD'''
-
-        '''NEURAL HAMILTONIAN-STLE VECTOR FIELD'''
-
         # step 1, get qdot back
-        # q = self.rebase(x[:,:2])
-        # qdot = (self.Minv@(x[:,2:].reshape(-1,1))).reshape(-1,2)
+        # q = self.rebase(x[:, :2])
+        # qdot = (self.Minv @ (x[:, 2:].reshape(-1, 1))).reshape(-1, 2)
         # Lpartials = self.Lpartials(torch.cat([q, qdot], 1))
-        # pdot = Lpartials[:,:2]
-        # return torch.cat([qdot,pdot],1)
+        # pdot = Lpartials[:, :2]
+        # return torch.cat([qdot, pdot], 1)
 
-        hp = self.Hpartials(x)
+        hp = self.Hpartials(torch.cat([self.rebase(x[:,:2]),x[:,2:]],1))
         return hp
