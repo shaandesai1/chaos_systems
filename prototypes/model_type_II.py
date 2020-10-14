fbdcfd78c47d6b9f44fbb50ab9aa0123a4aec037
Shaan Desai <shaandesai@live.com>
2020-10-14 14:50:42 +0200

    new prototypes

---
+++ prototypes/model_type_II.py
@@ -0,0 +1,71 @@
+import torch.nn as nn
+import torch
+import numpy as np
+
+class HNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_size, deltat):
+        super(HNN, self).__init__()
+        self.dt = deltat
+        self.mlp1 = nn.Linear(2, hidden_dim)
+        self.mlp2 = nn.Linear(hidden_dim, hidden_dim)
+        self.mlp3 = nn.Linear(hidden_dim, hidden_dim)
+        self.mlp4 = nn.Linear(hidden_dim, 1)
+        self.nonlin = torch.tanh
+        
+        
+        self.f1 = nn.Linear(1, hidden_dim)
+        self.f2 = nn.Linear(hidden_dim, hidden_dim)
+        self.f3 = nn.Linear(hidden_dim,1)
+        self.nonlin = torch.tanh
+        
+        
+        self.M = self.permutation_tensor(2)
+        for l in [self.mlp1, self.mlp2, self.mlp4,self.mlp3,self.f1,self.f2]:
+            torch.nn.init.orthogonal_(l.weight)
+
+    def rk4(self, dx_dt_fn, x_t,t):
+        k1 = self.dt * dx_dt_fn(x_t,t)
+        k2 = self.dt * dx_dt_fn(x_t + (1. / 2) * k1,t+.5*self.dt)
+        k3 = self.dt * dx_dt_fn(x_t + (1. / 2) * k2,t+.5*self.dt)
+        k4 = self.dt * dx_dt_fn(x_t + k3,t+self.dt)
+        x_tp1 = x_t + (1. / 6) * (k1 + k2 * 2. + k3 * 2. + k4)
+        return x_tp1
+
+    def get_H(self, x):
+        h = self.nonlin(self.mlp1(x))
+        h = self.nonlin(self.mlp2(h))
+        h = self.nonlin(self.mlp3(h))
+        fin = self.mlp4(h)
+        return fin
+
+    def get_F(self, x):
+        h = self.nonlin(self.f1(x))
+        h = self.nonlin(self.f2(h))
+        h= self.f3(h)
+        return h
+
+    def permutation_tensor(self, n):
+        M = torch.eye(n)
+        M = torch.cat([M[n // 2:], -M[:n // 2]])
+        return M
+
+    def next_step(self, x,t):
+        init_param = x
+        next_set = self.rk4(self.time_deriv, init_param,t)
+        return next_set
+
+    def time_deriv(self, x,t):
+#         input_vec = torch.cat([x, t.reshape(-1, 1)], 1)
+        H0,F = self.get_H(x),self.get_F(t.reshape(-1,1))
+#         print(H0.shape,F.x)
+        dF2 = torch.autograd.grad(H0.sum(), x, create_graph=True,allow_unused=True)[0]
+        dqdt = dF2[:,1]
+        dpdt = -dF2[:,0].reshape(-1,1) + F
+        return torch.cat([dqdt.reshape(-1,1), dpdt.reshape(-1,1)], 1)
+
+    def time_deriv2(self, x,t):
+        H0,F = self.get_H(torch.cat([x,t.reshape(-1,1)],1))
+        dHdt = torch.autograd.grad(H0.sum(), t, create_graph=True,allow_unused=True)[0]
+        dFdq = torch.autograd.grad(F.sum(), x[:,0], create_graph=True,allow_unused=True)[0]
+        dFdp = torch.autograd.grad(F.sum(), x[:,1], create_graph=True,allow_unused=True)[0]
+        return dHdt,dFdq,dFdp
